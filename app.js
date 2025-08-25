a2b96307e0a00594c9c226e261b5c62073fc2d51
najiyasheri <najiyasheri1273@gmail.com>
2025-08-25 17:11:53 +0530

    table and things brought

--- app.js
+++ app.js
@@ -3,13 +3,12 @@ const session=require('express-session')
 const env=require('dotenv').config()
 const passport = require("passport");
 require("./config/passport");
-
 const app=express()
 const path=require('path')
-
 const db=require('./config/db')
 const userRouter=require('./routes/user.routes')
 const adminRouter=require('./routes/admin.routes')
+
 db()
 
 
@@ -30,6 +29,9 @@ app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname,'public')))
+
+app.set("layout", "layouts/admin-layout");  
+
 
 app.use('/admin',adminRouter)
 app.use('/',userRouter)
